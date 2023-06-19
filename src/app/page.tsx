d9c8c0e4156a01ed7796da11feb1679d80be213a
William <euwilliamsantana@gmail.com>
2023-06-19 10:55:05 -0300

    feat: Post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
+'use client'
+
 import Image from 'next/image'
+import { ArrowBendDownRight } from 'phosphor-react'
 
 export default function Home() {
   return (
     <main className="grid place-items-center mx-2">
-      <header className="w-full flex justify-around items-end py-3 border border-black max-w-7xl m-3">
-        <a href="/" className="font-robotoMono text-xl text-brown-800">
+      <header className="w-full flex justify-around items-end py-3 border border-black max-w-7xl mt-4">
+        <a
+          href="/"
+          className="font-robotoMono text-xl text-brown-800 hover:scale-110 transition-all"
+        >
           Início
         </a>
         <h1 className="font-jacquesFracois text-4xl">Github</h1>
-        <a href="/" className="font-robotoMono text-xl text-brown-800">
+        <a
+          href="/"
+          className="font-robotoMono text-xl text-brown-800 hover:scale-110 transition-all"
+        >
           Sobre
         </a>
       </header>
 
-      <div className="flex max-w-[1120px] w-full gap-8 items-start">
+      <div className="flex max-w-[1120px] w-full gap-8 items-start mt-8">
         <div className="w-64 border border-black bg-brown-100">
           <Image
             src={
@@ -47,7 +56,38 @@ export default function Home() {
             </footer>
           </div>
         </div>
-        <div className="flex-1 border border-black"></div>
+        <div className="flex-1 border border-black px-8 bg-brown-100 pt-4 h-[292px]">
+          <div className="flex items-baseline justify-between">
+            <div className="flex gap-2 items-end">
+              <span className="text-xs">Projeto:</span>
+              <strong className="font-robotoMono font-bold">
+                Component-loading
+              </strong>
+            </div>
+            <time className="text-xs">Publicado a 1 mês</time>
+          </div>
+
+          <div className="bg-brown-300 border border-black mt-6 px-4 py-2 ">
+            <p className="text-sm">
+              Componente de carregamento! <br /> <br />
+              Confira o resultado em:
+              wwilliamsantana.github.io/component-loading/index.html <br /> 💻
+              Sobre <br />
+              Este projeto foi feito para práticar conceitos do JavaScript.
+              <br /> BOM (Browser Object Model)
+              <br /> DOM (Document Object Model)
+              <br />
+              Lógica.....
+            </p>
+          </div>
+          <a
+            href=""
+            className="flex justify-end text-sm font-light gap-2 my-1 hover:font-outline-1 transition-all"
+          >
+            <ArrowBendDownRight size={20} />
+            Visualizar conteúdo
+          </a>
+        </div>
       </div>
     </main>
   )
